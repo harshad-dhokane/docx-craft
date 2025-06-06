e9c72cc2c6b4e5ad646d2e4a9ad6e509ad90dbb8
harshad-dhokane <worrk.harsshad@gmail.com>
2025-06-06 17:29:38 +0530

    Final local working without api

--- client/src/pages/AuthCallback.tsx
+++ client/src/pages/AuthCallback.tsx
@@ -4,7 +4,7 @@ import { supabase } from '@/integrations/supabase/client';
 import { useToast } from '@/hooks/use-toast';
 
 const AuthCallback = () => {
-  const navigate = useNavigate();
+  const [, setLocation] = useLocation();
   const { toast } = useToast();
 
   useEffect(() => {
